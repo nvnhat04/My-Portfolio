641cfc8901106d2257b46d35d05c3399cb1bff9b
nvnhat04 <nvnhat04324@gmail.com>
2025-07-17 23:16:21 +0700

    chore: add lib and assets

---
+++ taiwind.config.mjs
@@ -0,0 +1,7 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class', // ⚠️ quan trọng!
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: { extend: {} },
+    plugins: [],
+}
